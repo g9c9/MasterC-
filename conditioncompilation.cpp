8ce2524d1f281cc8e77e87d42039f9301f6193fc
George <g9c9@outlook.com>
2022-06-18 22:46:56 -0700

    Conditions when macro gets replaced

--- conditioncompilation.cpp
+++ conditioncompilation.cpp
@@ -3,10 +3,17 @@
 // Object-like macro without substitution text
 #define PRINT_JOE
 
+// Object-like macro with substitution text
+#define FOO 9
+
 int main()
 {
 #ifdef PRINT_JOE
     std::cout << "Joe\n";   // will be compiled since PRINT_JOE is defined
+#endif
+
+#ifdef FOO // This FOO doesn't get replaced because it's part of another preprocessor directive
+    std::cout << FOO; // This FOO gets replaced with 9 because it;s part of the normal code
 #endif
 
 #ifdef PRINT_BOB
