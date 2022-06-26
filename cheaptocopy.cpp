c03819ea55e85ac154bde74f477db35c2ddd06c9
George <g9c9@outlook.com>
2022-06-25 22:32:08 -0700

    Cheap to copy instead of pass by ref

---
+++ cheaptocopy.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Macro: evaluates to true if the type (or object) uses 2 or fewer memory addresses worth of memory
+#define isSmall(T) (sizeof(T) <= 2 * sizeof(void*))
+
+struct S
+{
+    double a, b, c;
+};
+
+int main()
+{
+    std::cout << std::boolalpha; // print true or false rather than 1 or 0
+    std::cout << isSmall(int) << '\n'; // true
+    std::cout << isSmall(double) << '\n'; // true
+    std::cout << isSmall(S) << '\n'; // false
+    
+    return 0;
+}
